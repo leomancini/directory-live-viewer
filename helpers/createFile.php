25d2a7f23c0246dbcd8b252a28d210ed76109f07
Leo Mancini <mancini@leo.gd>
2020-07-11 17:05:17 -0700

    Cleaning up

--- helpers/createFile.php
+++ helpers/createFile.php
@@ -1,14 +1,14 @@
 <?php
     require('../config.php');
 
-    $inputFilename = basename($_FILES["image"]["name"]);
+    $inputFilename = basename($_FILES['image']['name']);
     $inputFileExtension = strtolower(pathinfo($directory.$inputFilename, PATHINFO_EXTENSION));
 
     $newFilename = $directory.$_POST['name'].'.'.$inputFileExtension;
 
-    if (move_uploaded_file($_FILES["image"]["tmp_name"], $newFilename)) {
-        echo "File uploaded: $newFilename";
+    if (move_uploaded_file($_FILES['image']['tmp_name'], $newFilename)) {
+        echo 'File uploaded: '.$newFilename;
     } else {
-        echo "Error uploading file";
+        echo 'Error uploading file';
     }
 ?>
